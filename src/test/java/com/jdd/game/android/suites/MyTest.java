a66ad23197c61db26901f066706c8df48af41eef
Administrator <Administrator@20170419-141915>
2017-09-04 17:41:35 +0800

    Update YAML

--- src/test/java/com/jdd/game/android/suites/MyTest.java
+++ src/test/java/com/jdd/game/android/suites/MyTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Listeners;
 import com.paypal.selion.annotations.MobileTest;
 import com.jdd.game.android.driver.IDriverExe;
 import com.jdd.game.android.report.MyReporter;
-import com.jdd.pages.LoginPage;
+import com.jdd.pages.DengLuPage;
 
 @Listeners({MyReporter.class})
 public class MyTest extends AbsParentTest {
@@ -16,12 +16,12 @@ public class MyTest extends AbsParentTest {
 	public void test1() {
 		String caseName = "测试登录";
 		IDriverExe driverExe = startTest(NAME_TEST, caseName);
-		LoginPage lp = new LoginPage();
-		driverExe.tapElement(lp.getPhoneLoginUiObject(), "点击手机登录");
-		driverExe.appendTextField(lp.getPhoneNumberUiTextView(), "13151815253");
-		driverExe.appendTextField(lp.getPasswordUiTextView(), "zhouzhou");
-		driverExe.clear2SetTextField(lp.getPhoneNumberUiTextView(), "13151815253");
-		driverExe.tapElement(lp.getLoginUiObject(), "登录确认");
+		DengLuPage dl = new DengLuPage();
+		driverExe.tapElement(dl.getShoujidengluUiObject(), "点击手机登录");
+		driverExe.appendTextField(dl.getShoujihaomaUiTextView(), "13151815253");
+		driverExe.appendTextField(dl.getMimaUiTextView(), "zhouzhou");
+		driverExe.clear2SetTextField(dl.getShoujihaomaUiTextView(), "13151815253");
+		driverExe.tapElement(dl.getDengluUiObject(), "登录确认");
 		driverExe.waitPageLoad(10);
 		endTest(NAME_TEST, caseName);
 	}
