739aa818bc3ed7cb562d582507f61d4c966236a9
Administrator <Administrator@20170419-141915>
2017-09-25 11:29:26 +0800

    Update

--- src/test/java/com/jdd/game/android/browser/AbsParentTest.java
+++ src/test/java/com/jdd/game/android/browser/AbsParentTest.java
@@ -4,7 +4,7 @@ import com.jdd.game.android.driver.BrowserDriver;
 import com.jdd.game.android.driver.IWapDriverExe;
 
 public abstract class AbsParentTest extends BrowserDriver{
-	private static final Integer WAIT_LOAD_GAME		= 3;
+	private static final Integer WAIT_LOAD_GAME		= 10;
 	protected static final String OKOOO_LOGIN		= "http://192.168.101.181/channel/newokooo/home/1102/#/loginPop";
 	protected static final String OKOOO_TOKEN		= "http://192.168.101.181/channel/newokooo/home/1102/?token=%s&type=jdd&status=login#/";
 	protected static final String OKOOO_YOU_XI		= "http://192.168.101.181/channel/newokooo/home/1102/#/";
